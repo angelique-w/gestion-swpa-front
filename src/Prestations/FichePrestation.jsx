3618e5c924ec30862c525adc819be6e7a9f8808e
angelique-w <angelique.wons@gmail.com>
2020-02-17 20:00:09 +0100

    completed fiche prestation

--- src/Prestations/FichePrestation.jsx
+++ src/Prestations/FichePrestation.jsx
@@ -11,7 +11,11 @@ import {
     RadioGroup,
     IconButton,
     InputLabel,
-    Select
+    Select,
+    TextField,
+    FormGroup,
+    Checkbox,
+    Button
 } from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
@@ -31,9 +35,10 @@ const useStyles = makeStyles(theme => ({
     formControl: {
         margin: theme.spacing(3)
     },
-    selectRole: {
-        margin: theme.spacing(1),
-        minWidth: 200
+    select: {
+        // margin: theme.spacing(1),
+        marginBottom: theme.spacing(5),
+        minWidth: 250
     }
 }));
 
@@ -42,6 +47,8 @@ function FichePrestation() {
     const [presence, setPresence] = useState("");
     const [accompagnants, setAccompagnants] = useState("");
     const [role, setRole] = useState("");
+    const [NbAccompagnants, setNbAccompagnants] = useState(0);
+    const [voiture, setVoiture] = useState(false);
 
     const handleChangePresence = event => {
         setPresence(event.target.value);
@@ -53,6 +60,14 @@ function FichePrestation() {
 
     const handleChangeRole = event => {
         setRole(event.target.value);
+    };
+
+    const handleChangeNbAccompagnants = event => {
+        setNbAccompagnants(event.target.value);
+    };
+
+    const handleChangeVoiture = event => {
+        setVoiture(!voiture);
     };
 
     return (
@@ -67,8 +82,8 @@ function FichePrestation() {
                     <ArrowBackIcon fontSize="large" />
                 </IconButton>
             </Box>
-            <Box my={5} mx={5}>
-                <Grid>
+            <Box my={5} mx={5} width="100%">
+                <Box mx={3}>
                     <Typography
                         variant="body1"
                         color="textSecondary"
@@ -97,91 +112,146 @@ function FichePrestation() {
                     >
                         Plus de détails...
                     </Typography>
-                </Grid>
-                <Grid xs={12}>
-                    <FormControl
-                        component="fieldset"
-                        className={classes.formControl}
-                    >
-                        <FormLabel component="legend">
-                            Déclarer ma présence
-                        </FormLabel>
-                        <RadioGroup
-                            aria-label="presence"
-                            name="presence"
-                            value={presence}
-                            onChange={handleChangePresence}
+                </Box>
+                <Grid
+                    // xs={6}
+                    container
+                    // direction="column"
+                    // justify="center"
+                    // alignItems="center"
+                >
+                    <Grid item xs={12}>
+                        <FormControl
+                            component="fieldset"
+                            className={classes.formControl}
                         >
-                            <Grid container>
-                                <Grid item>
-                                    <FormControlLabel
-                                        value="present"
-                                        control={<Radio />}
-                                        label="Présent"
-                                    />
-                                </Grid>
-                                <Grid item>
-                                    {presence === "present" && (
-                                        <FormControl
-                                            className={classes.selectRole}
-                                        >
-                                            <InputLabel htmlFor="role">
-                                                Présent en tant que ...
-                                            </InputLabel>
-                                            <Select
-                                                native
-                                                value={role}
-                                                onChange={handleChangeRole}
-                                                inputProps={{
-                                                    name: "role",
-                                                    id: "role"
-                                                }}
+                            <FormLabel component="legend">
+                                Déclarer ma présence
+                            </FormLabel>
+                            <RadioGroup
+                                aria-label="presence"
+                                name="presence"
+                                value={presence}
+                                onChange={handleChangePresence}
+                            >
+                                <Grid container direction="column">
+                                    <Grid item>
+                                        <FormControlLabel
+                                            value="present"
+                                            control={<Radio />}
+                                            label="Présent"
+                                        />
+                                    </Grid>
+                                    <Grid item>
+                                        {presence === "present" && (
+                                            <FormControl
+                                                className={classes.select}
                                             >
-                                                <option value="" />
-                                                <option value="piper">
-                                                    Piper
-                                                </option>
-                                                <option value="bbb">bbb</option>
-                                            </Select>
-                                        </FormControl>
-                                    )}
+                                                <InputLabel
+                                                    shrink
+                                                    htmlFor="role"
+                                                >
+                                                    Présent en tant que ...
+                                                </InputLabel>
+                                                <Select
+                                                    native
+                                                    value={role}
+                                                    onChange={handleChangeRole}
+                                                    inputProps={{
+                                                        name: "role",
+                                                        id: "role"
+                                                    }}
+                                                >
+                                                    <option value="" />
+                                                    <option value="piper">
+                                                        Piper
+                                                    </option>
+                                                    <option value="bbb">
+                                                        bbb
+                                                    </option>
+                                                </Select>
+                                            </FormControl>
+                                        )}
+                                    </Grid>
                                 </Grid>
-                            </Grid>
-                            <FormControlLabel
-                                value="absent"
-                                control={<Radio />}
-                                label="Absent"
-                            />
-                        </RadioGroup>
-                    </FormControl>
-                </Grid>
-                <Grid>
-                    <FormControl
-                        component="fieldset"
-                        className={classes.formControl}
-                    >
-                        <FormLabel component="legend">
-                            Viendrai-je accompagné(e) ?
-                        </FormLabel>
-                        <RadioGroup
-                            aria-label="accompagnants"
-                            name="accompagnants"
-                            value={accompagnants}
-                            onChange={handleChangeAccompagnants}
+                                <FormControlLabel
+                                    value="absent"
+                                    control={<Radio />}
+                                    label="Absent"
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl
+                            component="fieldset"
+                            className={classes.formControl}
                         >
+                            <FormLabel component="legend">
+                                Viendrai-je accompagné(e) ?
+                            </FormLabel>
+                            <RadioGroup
+                                aria-label="accompagnants"
+                                name="accompagnants"
+                                value={accompagnants}
+                                onChange={handleChangeAccompagnants}
+                            >
+                                <Grid container direction="column">
+                                    <Grid item>
+                                        <FormControlLabel
+                                            value="oui"
+                                            control={<Radio />}
+                                            label="Oui"
+                                        />
+                                    </Grid>
+                                    <Grid item>
+                                        {accompagnants === "oui" && (
+                                            <TextField
+                                                id="standard-number"
+                                                label="Number"
+                                                type="number"
+                                                value={NbAccompagnants}
+                                                onChange={
+                                                    handleChangeNbAccompagnants
+                                                }
+                                                inputProps={{
+                                                    min: "0",
+                                                    max: "5",
+                                                    step: "1"
+                                                }}
+                                                className={classes.select}
+                                            />
+                                        )}
+                                    </Grid>
+                                </Grid>
+                                <FormControlLabel
+                                    value="non"
+                                    control={<Radio />}
+                                    label="Non"
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormGroup row>
                             <FormControlLabel
-                                value="oui"
-                                control={<Radio />}
-                                label="Oui"
+                                control={
+                                    <Checkbox
+                                        checked={voiture}
+                                        onChange={handleChangeVoiture}
+                                        value="voiture"
+                                    />
+                                }
+                                label="Je prends ma voiture pour covoiturer"
                             />
-                            <FormControlLabel
-                                value="non"
-                                control={<Radio />}
-                                label="Non"
-                            />
-                        </RadioGroup>
-                    </FormControl>
+                        </FormGroup>
+                    </Grid>
                 </Grid>
+            </Box>
+            <Box textAlign="center" mb={5}>
+                <Button variant="contained" color="secondary">
+                    Valider
+                </Button>
             </Box>
         </Layout>
     );
